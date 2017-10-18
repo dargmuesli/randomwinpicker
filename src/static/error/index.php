794cc0b20257e87b0c185d160b74609460fae476
Jonas Thelemann <e-mail@jonas-thelemann.de>
2017-10-18 23:12:40 +0200

    Error Style
    ~ Moved error style css to external file

--- src/static/error/index.php
+++ src/static/error/index.php
@@ -8,28 +8,7 @@
         <link rel="icon" href="<?php echo 'https://' . $_SERVER['SERVER_NAME'] . '/layout/icons/favicon.ico'; ?>" type="image/png" />
         <link rel="stylesheet" href="<?php echo 'https://' . $_SERVER['SERVER_NAME'] . '/layout/stylesheets/fonts.css'; ?>">
         <link rel="stylesheet" href="<?php echo 'https://' . $_SERVER['SERVER_NAME'] . '/layout/stylesheets/style.css'; ?>">
-        <style type="text/css">
-            main {
-                position: fixed;
-                width: 100%;
-                height: 100%;
-                top: 0;
-
-                background-image: linear-gradient(rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.75)), url(<?php echo $_SERVER['SERVER_ROOT_URL']; ?>/layout/icons/eruption.jpg);
-                background-size: cover;
-                font-size: 200%;
-            }
-            main div {
-                height: 100%;
-                position: absolute;
-                top: 50%;
-                left: 50%;
-                transform: translate(-65%, -30%);
-            }
-            header a {
-                z-index: 1;
-            }
-        </style>
+        <link rel="stylesheet" href="<?php echo 'https://' . $_SERVER['SERVER_NAME'] . '/layout/stylesheets/error.css'; ?>">
     </head>
     <body>
         <header>
