8f187e48b97e7c8c18c7795b36240a4c18ad19cc
Jonas Thelemann <e-mail@jonas-thelemann.de>
2018-04-04 00:30:41 +0200

    Join-Path: Certificates
    + Join-Path variables

--- docker/conf/apache/cert/New-Certificates.ps1
+++ docker/conf/apache/cert/New-Certificates.ps1
@@ -3,11 +3,19 @@ Param (
     [String] $Password
 )
 
+$RootCer = Join-Path -Path $PSScriptRoot -ChildPath "root.cer"
+$RootCnf = Join-Path -Path $PSScriptRoot -ChildPath "root.cnf"
+$RootKey = Join-Path -Path $PSScriptRoot -ChildPath "root.key"
+$ServerCer = Join-Path -Path $PSScriptRoot -ChildPath "server.cer"
+$ServerCnf = Join-Path -Path $PSScriptRoot -ChildPath "server.cnf"
+$ServerCsr = Join-Path -Path $PSScriptRoot -ChildPath "server.csr"
+$ServerKey = Join-Path -Path $PSScriptRoot -ChildPath "server.key"
+
 Write-Host "Creating Root CA certificate & private key..." -ForegroundColor "Cyan"
-openssl req -x509 -new -config $PSScriptRoot\root.cnf -passout pass:$Password -out $PSScriptRoot\root.cer -keyout $PSScriptRoot\root.key
+openssl req -x509 -new -config $RootCnf -passout pass:$Password -out $RootCer -keyout $RootKey
 
 Write-Host "Creating server certificate & private key..." -ForegroundColor "Cyan"
-openssl req -nodes -new -config $PSScriptRoot\server.cnf -out $PSScriptRoot\server.csr -keyout $PSScriptRoot\server.key
+openssl req -nodes -new -config $ServerCnf -out $ServerCsr -keyout $ServerKey
 
 Write-Host "Signing with Root CA..." -ForegroundColor "Cyan"
-openssl x509 -req -in $PSScriptRoot\server.csr -CA $PSScriptRoot\root.cer -CAkey $PSScriptRoot\root.key -set_serial 123 -out $PSScriptRoot\server.cer -extfile $PSScriptRoot\server.cnf -extensions x509_ext -passin pass:$Password
+openssl x509 -req -in $ServerCsr -CA $RootCer -CAkey $RootKey -set_serial 123 -out $ServerCer -extfile $ServerCnf -extensions x509_ext -passin pass:$Password
