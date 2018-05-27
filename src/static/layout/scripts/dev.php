2fd0e7c27ed33c7e42ceeb681ec008a8a1ebea65
Jonas Thelemann <e-mail@jonas-thelemann.de>
2018-05-28 01:57:20 +0200

    TLD Test
    ~ Simplify check if site is live to TLD ("test") check

--- src/static/layout/scripts/dev.php
+++ src/static/layout/scripts/dev.php
@@ -1,12 +1,6 @@
 <?php
-    $liveDomains = ['randomwinpicker.de'];
-    $devDomains = ['localhost', '127.0.0.1', 'randomwinpicker.dev'];
-    $siteIsLive = null;
+    $siteIsLive = true;
 
-    if (in_array($_SERVER['HTTP_HOST'], $liveDomains)) {
-        $siteIsLive = true;
-    } elseif (in_array($_SERVER['HTTP_HOST'], $devDomains)) {
+    if (end(explode('.', $_SERVER['HTTP_HOST'])) == 'test') {
         $siteIsLive = false;
-    } else {
-        throw new Exception('Unknown domain!');
     }
