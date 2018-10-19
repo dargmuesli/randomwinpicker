a5b0fe4a57038e9ac099d19f46be4ecf39e2df57
Jonas Thelemann <e-mail@jonas-thelemann.de>
2018-10-19 15:26:26 +0200

    Dotenv Environment
    ~ Replace dotenv.php with environment.php

--- src/static/resources/dargmuesli/dotenv.php
+++
@@ -1,5 +0,0 @@
-<?php
-    require_once $_SERVER['DOCUMENT_ROOT'] . '/resources/packages/composer/autoload.php';
-
-    $dotenv = new Dotenv\Dotenv(dirname($_SERVER['DOCUMENT_ROOT']).'/credentials');
-    $dotenv->load();
