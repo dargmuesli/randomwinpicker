c303462da877fdd0c158a2289315a950aaa452ec
Jonas Thelemann <e-mail@jonas-thelemann.de>
2018-10-20 02:48:55 +0200

    ReCaptcha Fixes
    + Load environment variable file
    ~ Correct includes

--- src/static/resources/dargmuesli/recaptcha.php
+++ src/static/resources/dargmuesli/recaptcha.php
@@ -1,11 +1,14 @@
 <?php
-    require_once $_SERVER['DOCUMENT_ROOT'] . '/resources/packages/composer/autoload.php';
-    require_once $_SERVER['DOCUMENT_ROOT'] . '/layout/scripts/dev.php';
-    require_once $_SERVER['DOCUMENT_ROOT'] . '/layout/scripts/dotenv.php';
+    require_once $_SERVER['DOCUMENT_ROOT'].'/resources/packages/composer/autoload.php';
+    include_once $_SERVER['DOCUMENT_ROOT'].'/resources/dargmuesli/dev.php';
+    include_once $_SERVER['DOCUMENT_ROOT'].'/resources/dargmuesli/filesystem/environment.php';
 
     function get_recaptcha()
     {
         global $siteIsLive;
+
+        // Load .env file
+        load_env_file($_SERVER['SERVER_ROOT'].'/credentials');
 
         if ($siteIsLive) {
             return new ReCaptcha\ReCaptcha($_ENV['RECAPTCHA_SECRET']);
@@ -17,6 +20,9 @@
     function get_recaptcha_sitekey()
     {
         global $siteIsLive;
+
+        // Load .env file
+        load_env_file($_SERVER['SERVER_ROOT'].'/credentials');
 
         if ($siteIsLive) {
             return $recaptchaSiteKey = $_ENV['RECAPTCHA_SITEKEY'];
