0390130ec96b238f623866ce992a1ca3039ea6e3
Jonas Thelemann <e-mail@jonas-thelemann.de>
2019-03-11 18:44:17 +0100

    Rename Environment Variable File

--- src/static/resources/dargmuesli/filesystem/environment.php
+++ src/static/resources/dargmuesli/filesystem/environment.php
@@ -1,7 +1,7 @@
 <?php
     include_once $_SERVER['DOCUMENT_ROOT'].'/resources/packages/composer/autoload.php';
 
-    function load_env_file($folder, $file = '.env', $override = false)
+    function load_env_file($folder, $file = 'randomwinpicker.env', $override = false)
     {
         $dotenv = new Dotenv\Dotenv($folder, $file);
         ($override) ? $dotenv->overload() : $dotenv->load();
